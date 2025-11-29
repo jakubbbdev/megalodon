42978ea439d4be5123bb830c6f33ef79e7fc5ac3
!verity <veritycsgo@gmail.com>
2025-11-29 01:25:48 +0100

    Add enums for inventory rows and slots
    - Introduced `InventoryRows` enum to represent row counts with corresponding slot values.
    - Added `InventorySlots` enum to define individual slot positions for better inventory management.

---
+++ src/main/kotlin/dev/jakub/megalodon/ui/data/InventoryRows.kt
@@ -0,0 +1,10 @@
+package dev.jakub.megalodon.dev.jakub.megalodon.ui.data
+
+enum class InventoryRows(val value: Int) {
+    ROW1(9),
+    ROW2(18),
+    ROW3(27),
+    ROW4(36),
+    ROW5(45),
+    ROW6(54),
+}
